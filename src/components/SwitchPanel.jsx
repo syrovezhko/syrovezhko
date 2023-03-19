9312a18a08876396933dcc38597c28e0b205c6f5
Nikolai Syrovezhko <syrovezhko@list.ru>
2023-03-19 16:30:29 +0300

    feat: CV `download` button

--- src/components/SwitchPanel.jsx
+++ src/components/SwitchPanel.jsx
@@ -3,6 +3,7 @@ import eng from '../assets/icon/eng.png'
 import rus from '../assets/icon/rus.png'
 import day from '../assets/icon/day.png'
 import night from '../assets/icon/night.png'
+import download from '../assets/icon/download.svg'
 
 const SwitchPanel = ({term, lang}) => {
   return (
@@ -18,6 +19,11 @@ const SwitchPanel = ({term, lang}) => {
           ? <img src={day} alt="rus" />
           : <img src={night} alt="rus" />
         }
+      </li>
+      <li className="header--switch-download">
+        <a href="https://hh.ru/resume_converter/%D0%A1%D1%8B%D1%80%D0%BE%D0%B2%D0%B5%D0%B6%D0%BA%D0%BE%20%D0%9D%D0%B8%D0%BA%D0%BE%D0%BB%D0%B0%D0%B9%20%D0%90%D0%BB%D0%B5%D0%BA%D1%81%D0%B5%D0%B5%D0%B2%D0%B8%D1%87.pdf?hash=32369b73ff08cb73dc0039ed1f35596a467a4d&type=pdf&hhtmSource=resume&hhtmFrom=resume_list" download>
+          <img src={download} alt="download" />
+        </a>
       </li>
     </ul>
   );
